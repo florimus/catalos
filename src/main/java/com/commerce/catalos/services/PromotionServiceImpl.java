5a4e8e597d1e3d6ad4b1c801ed95150eb6b14eba
Anoopss <anoopviswanadh007@gmail.com>
2025-06-08 00:44:18 +0530

    create flatOFF

--- src/main/java/com/commerce/catalos/services/PromotionServiceImpl.java
+++ src/main/java/com/commerce/catalos/services/PromotionServiceImpl.java
@@ -35,6 +35,7 @@ public class PromotionServiceImpl implements PromotionService {
     public CreatePromotionResponse createPromotion(final CreatePromotionRequest createPromotionRequest) {
         Discount discount = switch (createPromotionRequest.getDiscountType()) {
             case PercentageOFF -> this.createPercentageOFFDiscount(createPromotionRequest);
+            case FlatOFF -> this.createFlatOFFDiscount(createPromotionRequest);
             default ->
                 throw new IllegalArgumentException("Unexpected value: " + createPromotionRequest.getDiscountType());
         };
@@ -53,34 +54,19 @@ public class PromotionServiceImpl implements PromotionService {
         channelService.verifyChannels(List.of(channelId), true);
     }
 
-    public Discount createPercentageOFFDiscount(final CreatePromotionRequest createPromotionRequest) {
-        Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Validating Percentage Off Discount {}",
+    public Discount createFlatOFFDiscount(final CreatePromotionRequest createPromotionRequest) {
+        Logger.info("8013b8ff-a153-423c-ac1b-f647cc7fe609", "Validating Flat Off Discount {}",
                 createPromotionRequest.getName());
         Discount discount = new Discount();
         discount.setName(createPromotionRequest.getName());
         discount.setDiscountMode(createPromotionRequest.getDiscountMode());
         discount.setDiscountType(createPromotionRequest.getDiscountType());
-
-        if (createPromotionRequest.getDiscountValue() == null
-                || createPromotionRequest.getDiscountValue() <= 0 || createPromotionRequest.getDiscountValue() > 100) {
-            Logger.error("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Invalid discount value: {}",
-                    createPromotionRequest.getDiscountValue());
-            throw new BadRequestException("Invalid discount value");
-        }
-
-        Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Setting discount value to {}",
-                createPromotionRequest.getDiscountValue());
         discount.setDiscountValue(createPromotionRequest.getDiscountValue());
-
-        if (createPromotionRequest.getMaxDiscountPrice() != null) {
-            Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Setting max discount price to {}",
-                    createPromotionRequest.getMaxDiscountPrice());
-            discount.setMaxDiscountPrice(createPromotionRequest.getMaxDiscountPrice());
-        }
+        discount.setMaxDiscountPrice(createPromotionRequest.getMaxDiscountPrice());
 
         if (createPromotionRequest.getDiscountedProducts() != null
                 && !createPromotionRequest.getDiscountedProducts().isEmpty()) {
-            Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Setting discounted products to {}",
+            Logger.info("574a0e91-f38e-4bfc-b5a9-98be719ef2f2", "Setting discounted products to {}",
                     createPromotionRequest.getDiscountedProducts());
             discount.setDiscountedProducts(this.validateProductIds(createPromotionRequest.getDiscountedProducts()));
         }
@@ -90,7 +76,94 @@ public class PromotionServiceImpl implements PromotionService {
 
         if (createPromotionRequest.getTargetedProductIds() != null
                 && !createPromotionRequest.getTargetedProductIds().isEmpty()) {
-            Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Setting targeted products to {}",
+            Logger.info("be380ae6-f592-4e5e-b58c-1a6f879d5d51", "Setting targeted products to {}",
+                    createPromotionRequest.getTargetedProductIds());
+            discount.setTargetedProductIds(this.validateProductIds(createPromotionRequest.getTargetedProductIds()));
+        }
+
+        if (createPromotionRequest.getTargetedVariantIds() != null
+                && !createPromotionRequest.getTargetedVariantIds().isEmpty()) {
+            Logger.info("dee18d65-1a1a-415c-8bab-059bb48aefe0", "Setting targeted variants to {}",
+                    createPromotionRequest.getTargetedVariantIds());
+            discount.setTargetedVariantIds(this.validateVariantIds(createPromotionRequest.getTargetedVariantIds()));
+        }
+
+        // TODO: Implement the logic verify categories
+        // TODO: Implement the logic verify collections
+        // TODO: Implement the logic verify brands
+        // TODO: Implement the logic verify customer groups
+
+        if (TimeUtils.isFutureDate(createPromotionRequest.getStartDate())) {
+            Logger.info("4b8689d3-3bd1-47ba-933d-695a6391449f", "Setting start date to {}",
+                    createPromotionRequest.getStartDate());
+            discount.setStartDate(createPromotionRequest.getStartDate());
+        } else {
+            Logger.error("04a8bdb4-7ed1-42bb-968d-f5f661234c4f", "Start date cannot be in the past");
+            throw new BadRequestException("Start date cannot be in the past");
+        }
+
+        if (TimeUtils.isFutureDate(createPromotionRequest.getExpireDate()) &&
+                createPromotionRequest.getExpireDate().after(createPromotionRequest.getStartDate())) {
+            Logger.info("e857d8d4-9df6-48d4-a4a0-18cd5dcca1f8", "Setting expire date to {}",
+                    createPromotionRequest.getExpireDate());
+            discount.setExpireDate(createPromotionRequest.getExpireDate());
+        } else {
+            Logger.error("6b37abea-6689-4e16-9018-c52592af90b6", "Expire date cannot be in the past");
+            throw new BadRequestException("Expire date cannot be in the past");
+        }
+
+        this.isValidChannel(createPromotionRequest.getAvailableChannel());
+        Logger.info("af6f051c-b0ad-4157-ba22-a8f679ccc5ec", "Setting available channel to {}",
+                createPromotionRequest.getAvailableChannel());
+        discount.setAvailableChannel(createPromotionRequest.getAvailableChannel());
+
+        discount.setCreatedBy(authContext.getCurrentUser().getEmail());
+        discount.setCreatedAt(new Date());
+        discount.setEnabled(true);
+        discount.setActive(true);
+        Logger.info("fd0ac589-0603-4fa3-9ddf-45428ffeb38ba", "Creating discount with name: {}",
+                createPromotionRequest.getName());
+        return promotionRepository.save(discount);
+    }
+
+    public Discount createPercentageOFFDiscount(final CreatePromotionRequest createPromotionRequest) {
+        Logger.info("089a0eaf-0436-48b6-ae44-e3415b313793", "Validating Percentage Off Discount {}",
+                createPromotionRequest.getName());
+        Discount discount = new Discount();
+        discount.setName(createPromotionRequest.getName());
+        discount.setDiscountMode(createPromotionRequest.getDiscountMode());
+        discount.setDiscountType(createPromotionRequest.getDiscountType());
+
+        if (createPromotionRequest.getDiscountValue() == null
+                || createPromotionRequest.getDiscountValue() <= 0 || createPromotionRequest.getDiscountValue() > 100) {
+            Logger.error("2bb37130-487c-4c20-abc6-88e5e3a15d98", "Invalid discount value: {}",
+                    createPromotionRequest.getDiscountValue());
+            throw new BadRequestException("Invalid discount value");
+        }
+
+        Logger.info("950e2ee3-490d-455f-a659-63a1a592444a", "Setting discount value to {}",
+                createPromotionRequest.getDiscountValue());
+        discount.setDiscountValue(createPromotionRequest.getDiscountValue());
+
+        if (createPromotionRequest.getMaxDiscountPrice() != null) {
+            Logger.info("018cf16e-08c2-47c6-b5ef-e147ed5ed9ed", "Setting max discount price to {}",
+                    createPromotionRequest.getMaxDiscountPrice());
+            discount.setMaxDiscountPrice(createPromotionRequest.getMaxDiscountPrice());
+        }
+
+        if (createPromotionRequest.getDiscountedProducts() != null
+                && !createPromotionRequest.getDiscountedProducts().isEmpty()) {
+            Logger.info("7012dbea-aa0f-4d1b-b8b6-1a08d08ab3f3", "Setting discounted products to {}",
+                    createPromotionRequest.getDiscountedProducts());
+            discount.setDiscountedProducts(this.validateProductIds(createPromotionRequest.getDiscountedProducts()));
+        }
+
+        discount.setMinItemQuantity(createPromotionRequest.getMinItemQuantity());
+        discount.setForAllProducts(createPromotionRequest.isForAllProducts());
+
+        if (createPromotionRequest.getTargetedProductIds() != null
+                && !createPromotionRequest.getTargetedProductIds().isEmpty()) {
+            Logger.info("6350320a-065c-4e81-b815-e4df12b5f912", "Setting targeted products to {}",
                     createPromotionRequest.getTargetedProductIds());
             discount.setTargetedProductIds(this.validateProductIds(createPromotionRequest.getTargetedProductIds()));
         }
@@ -108,26 +181,26 @@ public class PromotionServiceImpl implements PromotionService {
         // TODO: Implement the logic verify customer groups
 
         if (TimeUtils.isFutureDate(createPromotionRequest.getStartDate())) {
-            Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Setting start date to {}",
+            Logger.info("00cc6b3a-9f22-4377-9b43-6f649c73ff3b", "Setting start date to {}",
                     createPromotionRequest.getStartDate());
             discount.setStartDate(createPromotionRequest.getStartDate());
         } else {
-            Logger.error("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Start date cannot be in the past");
+            Logger.error("fdf88672-c34e-4151-bb3f-0ec86ce34f4c", "Start date cannot be in the past");
             throw new BadRequestException("Start date cannot be in the past");
         }
 
         if (TimeUtils.isFutureDate(createPromotionRequest.getExpireDate()) &&
                 createPromotionRequest.getExpireDate().after(createPromotionRequest.getStartDate())) {
-            Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Setting expire date to {}",
+            Logger.info("40dcf8a4-e9b1-4287-b518-a2541237b9bf", "Setting expire date to {}",
                     createPromotionRequest.getExpireDate());
             discount.setExpireDate(createPromotionRequest.getExpireDate());
         } else {
-            Logger.error("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Expire date cannot be in the past");
+            Logger.error("997e3c61-63ee-4e1b-8d56-60a5f3d4f9cd", "Expire date cannot be in the past");
             throw new BadRequestException("Expire date cannot be in the past");
         }
 
         this.isValidChannel(createPromotionRequest.getAvailableChannel());
-        Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Setting available channel to {}",
+        Logger.info("74219d66-6f01-44a6-8979-17993acb9812", "Setting available channel to {}",
                 createPromotionRequest.getAvailableChannel());
         discount.setAvailableChannel(createPromotionRequest.getAvailableChannel());
 
@@ -135,7 +208,7 @@ public class PromotionServiceImpl implements PromotionService {
         discount.setCreatedAt(new Date());
         discount.setEnabled(true);
         discount.setActive(true);
-        Logger.info("91553c66-6d0c-44d6-bd38-5f3efa490d6d", "Creating discount with name: {}",
+        Logger.info("8aa9fad3-9a96-411e-a599-03c89e3e5bca", "Creating discount with name: {}",
                 createPromotionRequest.getName());
         return promotionRepository.save(discount);
     }
