8e9aecb2000f554852d50edb315d726a6f4fc289
Anoopss <anoopviswanadh007@gmail.com>
2025-06-28 01:04:06 +0530

    tax price in price info

--- src/main/java/com/commerce/catalos/models/prices/TaxClassItem.java
+++ src/main/java/com/commerce/catalos/models/prices/TaxClassItem.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PriceInfo {
+public class TaxClassItem {
 
-    private List<TaxClassItem> taxClasses;
+    private String id;
 
-    private float salesPrice;
+    private String name;
 }
