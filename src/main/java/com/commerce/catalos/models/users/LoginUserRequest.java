d177ffdcb61e18d768d69034ee4e0a09fb559c91
Anoopss <anoopviswanadh007@gmail.com>
2025-08-01 23:55:09 +0530

    login tokens

--- src/main/java/com/commerce/catalos/models/users/LoginUserRequest.java
+++ src/main/java/com/commerce/catalos/models/users/LoginUserRequest.java
@@ -3,9 +3,11 @@ package com.commerce.catalos.models.users;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginUserRequest {
 
     @Email(message = "Invalid email format")
--- src/main/java/com/commerce/catalos/resolvers/UserResolvers.java
+++ src/main/java/com/commerce/catalos/resolvers/UserResolvers.java
@@ -1,9 +1,12 @@
 package com.commerce.catalos.resolvers;
 
 import com.commerce.catalos.core.configurations.Logger;
+import com.commerce.catalos.models.users.LoginUserRequest;
 import com.commerce.catalos.models.users.UserInfoResponse;
+import com.commerce.catalos.models.users.UserTokenResponse;
 import com.commerce.catalos.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -17,5 +20,11 @@ public class UserResolvers {
     public UserInfoResponse me() {
         Logger.info("", "Received request for my info");
         return userService.myInfo();
+    }
+
+    @QueryMapping
+    public UserTokenResponse loginUser(@Argument("email") final String email, @Argument("password") final String password) {
+        LoginUserRequest loginUserRequest = new LoginUserRequest(email, password);
+        return userService.loginUser(loginUserRequest);
     }
 }
